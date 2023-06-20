e055caa03f002ac464974163d9ec427fc5148988
Aditya Prakash <112635806+Adityaprakash754@users.noreply.github.com>
2023-06-20 10:48:04 +0000

    new features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,31 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes.js";
+
+// function createEntry(item) {
+//   return(
+//     <Note 
+//     key = {item.key}
+//     title = {item.title}
+//     content = {item.content}
+//     />
+//   );
+// }
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+
+      {/* {notes.map(createEntry)} */}
+      {notes.map(item => (<Note 
+            key = {item.key}
+            title = {item.title}
+            content = {item.content}
+            />
+        ))}
+  
       <Footer />
     </div>
   );
